468467407620733b2614cbf980e649987f2098bf
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-09-23 10:35:02 +0930

    chore: add require

--- tsup.config.ts
+++ tsup.config.ts
@@ -12,4 +12,13 @@ export default defineConfig({
     entry: {
         index: './src/index.ts'
     },
+    banner: {
+        js: `
+    // BANNER START
+    const require = (await import("node:module")).createRequire(import.meta.url);
+    const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
+    const __dirname = (await import("node:path")).dirname(__filename);
+    // BANNER END
+    `,
+    },
 });
